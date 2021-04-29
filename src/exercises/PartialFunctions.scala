92624a8d34c8f23c3ded3748461135f26bd85aa4
Dror Gilboa <dror@lightricks.com>
2021-04-29 22:10:07 +0300

    Add PartialFunctions

---
+++ src/exercises/PartialFunctions.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2021 Lightricks. All rights reserved.
+ */
+package exercises
+
+object PartialFunctions extends App {
+
+  val myPartialFunc = new PartialFunction[Int, Int] {
+    override def isDefinedAt(x: Int): Boolean = Set(1, 5).contains(x)
+
+    override def apply(v1: Int): Int = v1 match {
+      case 1 => 5
+      case 5 => 7
+    }
+  }
+
+  println(myPartialFunc(5))
+  println(myPartialFunc.lift(8).getOrElse(9))
+
+  /* Chat Bot with Partial Functions */
+  // scala.io.Source.stdin.getLines().foreach(line => println("you said: " + line))
+  List("whats up", "not me").map{
+    case "me" => "you"
+    case "not me" => "not you"
+    case "whats up" => "what u said"
+  }.foreach(println)
+}
